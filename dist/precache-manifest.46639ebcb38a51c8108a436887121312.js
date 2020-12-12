96127c588c324d0a9f06e396f2917b487cb5b990
alexander-vera <jalexanderv.meneses@gmail.com>
2020-12-12 18:24:10 -0500

    Fisrt deploy

---
+++ dist/precache-manifest.46639ebcb38a51c8108a436887121312.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25376d2af73e6af0b5c4af1438d3545d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6eac6af9f10a9ab0e69",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9bb11f6a14c36da53dbc",
+    "url": "/static/css/main.77eeeb20.chunk.css"
+  },
+  {
+    "revision": "b6eac6af9f10a9ab0e69",
+    "url": "/static/js/2.ec8a0a3d.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "/static/js/2.ec8a0a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb11f6a14c36da53dbc",
+    "url": "/static/js/main.095a1623.chunk.js"
+  },
+  {
+    "revision": "a8b47cda0a070d512323",
+    "url": "/static/js/runtime-main.dd618b6a.js"
+  }
+]);
